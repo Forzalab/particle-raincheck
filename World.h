aaa43494f6eba21db4a521270de3fae5cf556930
0865098 <0865098@my.scccd.edu>
2026-02-14 11:47:26 -0800

    Updated README.md to mark tasks I've completed, as well as REQUIREMENTS so that it's clear what's completed and what isn't. Worked on implementing World::map, World::physics(), World::alive_count(), World::size(). Currently untested due to compiler error that needs resolving in Particle.h. I am unsure of the fix currently, and it is necessary to test my code.

--- World.h
+++ World.h
@@ -26,16 +26,20 @@ private:
 	// derived 'Particle' type
 
  
+	void updateMap();
 	void parseParticlesFromJSON(std::string &s);
 
 public:
-	World(const Wc& rows, const Wc& cols) : rows(rows), cols(cols) {};
+	//Reserve mem for map in construction. 
+	World(const Wc& rows, const Wc& cols) : rows(rows), cols(cols) { map.resize(size_t(rows) * size_t(cols)); }
 
 	Wc get_rows() const; Wc get_cols() const;
 	void set_rows(const Wc& _rows); void set_cols(const Wc& _cols);
 
 	void erase(const Wc& row, const Wc& col);
 	P_ptr at(const Wc& row, const Wc& col) const; // .at() 
+	// Helper func to make World::physics() cleaner
+	bool isInBounds(const auto &p);
 	void physics(); // physics() iterates all P.
 	Amt size() const; // get amt of P
 	Amt alive_count() const; // get amt of LIVING P.
