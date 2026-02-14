fbcb9524e261cdc00753ee9fc53ab6d86f1e41f5
ho_1041841 <1041841@my.scccd.edu>
2026-02-13 19:53:28 -0800

    Split physics into general and specific (with rename), recleanup the files, and bug fixes

--- Particle.cc
+++ Particle.cc
@@ -1,71 +1,11 @@
-
-/*
-the due date you need to have:
-1. A Particle class. You must use 'proper' class design. Each
-part
-icle has:
---A position (.row, .col float) which indicates where it is on
- the
- screen
-----No two Particles can be on the same location
---A velocity (.x_vel and .y_vel float) indicating how many cel
-ls p
-er tick it moves
---A .type which must be an enum which indicates if it is air,
-dust
-, fire, water, earth, dirt, lightning
-----Each type has a different physics movement type:
-------Air moves in a straight line (ignoring gravity) bouncing
- off
- solid
-------Dust has a small amount of gravity and randomly moves le
-ft a
-nd right every frame
-------Fire is stationary and shoots sparks of lightning in dif
-fere
-nt directions over time
-------Water drips down and if it hits something solid it will
-slid
-e sideways to find the lowest level. Water touching fire turns
- int
-o air moving upwards
-------Earth is always stationary and solid
-------Lightning travels in a straight line and stops when it h
-its
-something solid. If it touches water, the water turns into lig
-htni
-ng. If it hits earth it turns into dirt
-------Dirt travels downwards and forms piles when it hits some
-thin
-g solid
-------You can add your own types as well for extra credit
---Color (.r .g .b uint8_t) that should be set initially by its
- typ
-e (fire should be red) but can change
---A boolean named .stationary that if true means it is solid a
-nd d
-oes not move/simulate physics
-----So if you turn stationary true on water it will not move e
-ven
-if a spot opens up beneath it
-----Stationary Particles can still be destroyed or transformed
- by
-other Particles
-----Stationary particles have a lifetime of -1 (see below)
---A .lifetime variable which holds how many frames more it wil
-l ex
-ist for
-----Physics (see below) will decrement it each frame by 1 to a
- min
-imum of 0.
-*/
-#include <cstdlib>
-#include <stdexcept>
-#include <ctime>
-#include <random>
 #include "Particle.h"
+#include "World.h"
 
 using P = Particle;
+
+std::random_device P::rd;
+std::mt19937 P::gen(P::rd());
+std::binomial_distribution<> P::bd(50, 0.5);
 
 // getters
 Pc P::get_row() const { return row; }
@@ -85,11 +25,13 @@ void P::set_row(const Pc &_row) {
 		throw std::runtime_error("Row OOB.");
 	row = _row;
 }
+
 void P::set_col(const Pc &_col) {
 	if (_col < 0)
 		throw std::runtime_error("Col OOB.");
 	col = _col;
 }
+
 void P::set_x_vel(const Pc &_x_vel) { x_vel = _x_vel; }
 void P::set_y_vel(const Pc &_y_vel) { y_vel = _y_vel; }
 void P::set_r(const Color &_r) { r = _r; }
@@ -102,56 +44,72 @@ void P::set_stationary(const bool &_stationary) {
 		set_y_vel(0);
 	}
 }
+
 void P::set_lifetime(const Tick &_lifetime) {
 	if (_lifetime < -1)
 		throw std::runtime_error("Lifetime OOB.");
 	lifetime = _lifetime;
 }
+
 void P::set_type(const Type &_type) {
 	if (!((E_MIN <= _type) && (_type <= E_MAX)))
 		throw std::runtime_error("Type OOB.");
 	type = _type;
 }
 
+void P::physics(World &world) {
+	const P *_p = world.at(get_row(), get_col());
+	if (_p != nullptr) {
+		// general guard for all particles
+		/* here */
+
+		// lifetime expiry check
+		if (get_lifetime() == 0) {
+			world.erase(get_row(), get_col());
+			return;
+		}
+
+		// type-specific physics
+		physics_spec(world);
+
+		// post specific-physics
+		set_lifetime(get_lifetime() - 1);
+	}
+
+	// DO NOT INPLMENT ANYTHING IN THIS SPACE
+	return;
+}
+
 // TODO: add constructor WITH MEM-INIT-LIST
 //       (r,g,b, stationary, lifetime) to definitions
+void Air::physics_spec(World &world) {
 
-void Air::physics(World &world) {
-	P* p = world.at(row, col); // cur pos
-	
-	// lifetime expiry check
-	if (lifetime == 0) {
-		world.ps.remove(p);
-		world.map.erase(row+col*world.cols);
-		return;
-	}
-	
-	// if not (not st.) = stay still 
+	// if not (not st.) = stay still
 	// ==> change nothing about pos
-	if (!stationary) {
-		p->set_row(p.row + x_vel);
-		p->set_col(p.col + y_vel);
+	if (!get_stationary()) {
+		set_row(get_row() + get_x_vel());
+		set_col(get_col() + get_y_vel());
 	}
 
-	set_lifetime(lifetime-1);
+	return;
 }
 
 void Air::touch(P &nbr) {}
-void Dust::physics(World &world) {}
+void Dust::physics_spec(World &world) {}
 void Dust::touch(P &nbr) {}
-void Fire::physics(World &world) {}
+void Fire::physics_spec(World &world) {}
 void Fire::touch(P &nbr) {}
-void Water::physics(World &world) {}
+void Water::physics_spec(World &world) {}
 void Water::touch(P &nbr) {}
-void Earth::physics(World &world) {}
+void Earth::physics_spec(World &world) {}
 void Earth::touch(P &nbr) {}
-void Dirt::physics(World &world) {}
+void Dirt::physics_spec(World &world) {}
 void Dirt::touch(P &nbr) {}
-void Lightning::physics(World &world) {}
+void Lightning::physics_spec(World &world) {}
 void Lightning::touch(P &nbr) {}
-void TBD_1::physics(World &world) {}
+void TBD_1::physics_spec(World &world) {}
 void TBD_1::touch(P &nbr) {}
-void TBD_2::physics(World &world) {}
+void TBD_2::physics_spec(World &world) {}
 void TBD_2::touch(P &nbr) {}
-void TBD_3::physics(World &world) {}
+void TBD_3::physics_spec(World &world) {}
 void TBD_3::touch(P &nbr) {}
--- World.h
+++ World.h
@@ -1,29 +1,3 @@
-/*
-World class that has a 2D grid of Particles (or are empty). W
-orld has:
---A size (.rows .cols int) that is the boundary of the world.
-----Any point which leaves the boundary must be deleted
---A map (rows x cols in size of chars) which indicates which parti
-cle type (or nothing) is at each place in the game world
---A list (must be std::list) of Particles which holds all particle
-s in the world
---An .at(row,col) method that returns a pointer to the Particle at
- the location row x col in the world (or nullptr if there is no pa
-rticle there)
---A .physics() method which runs through the list of Particles and
- runs .physics() on each one
-----If a particle is now out of bounds (no YOU'RE out of bounds) t
-hen delete it from the list
-----If a particle now has a lifetime of exactly 0, delete it from
-the list
---A .size() method which returns how many particles are in the wor
-ld
---A .alive_count() method which returns how many particles with li
-fetime > 0 are in the world
---A .save() method which saves the state of the world to disk
---A .load() method which loads the state of the world to disk
-*/
-
 #ifndef WORLD_H
 #define WORLD_H
 
@@ -36,14 +10,14 @@ fetime > 0 are in the world
 
 using P = Particle;
 
-typedef uint32_t Pos;
+typedef uint32_t Wc; // World-coords
 typedef int32_t Amt;
 typedef std::list<P*> Ps; // ParticleS
-typedef std::map<Amt, Type> Map;
+typedef std::vector<Type> Map;
 
 class World {
 private:
-	Pos rows{}, cols{}; // WxH of World
+	Wc rows{}, cols{}; // WxH of World
 	Map map; // FLATTENED map
 	Ps ps; // list of Particles' POINTERS
 	// Pointer is used for flexible intercasting to
@@ -53,8 +27,13 @@ private:
 	void parseParticlesFromJSON(std::string &s);
 
 public:
-	World(Pos rows, Pos cols) : rows(rows), cols(cols);
-	P* at(const Pos& row, const Pos& col) const; // .at() 
+	Wc get_rows() const; Wc get_cols() const;
+	void set_rows(const Wc& _rows); void set_cols(const Wc& _cols);
+
+	World(const Wc& rows, Wc& cols) : rows(rows), cols(cols) {};
+
+	void erase(const Wc& row, const Wc& col);
+	P* at(const Wc& row, const Wc& col) const; // .at() 
 	void physics(); // physics() iterates all P.
 	Amt size() const; // get amt of P
 	Amt alive_count() const; // get amt of LIVING P.
