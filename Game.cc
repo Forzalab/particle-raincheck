5c32b519b3d8e64db36c411f1cfa10d312052f73
0865098 <0865098@my.scccd.edu>
2026-02-15 18:20:10 -0800

    Fixes to Game::render() and a few other things. Currently FPS is capped at 1 for testing, can be changed.

--- Game.cc
+++ Game.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <thread>
 #include "Game.h"
 #include "/public/colors.h"
@@ -18,25 +19,29 @@ void Game::run() {
 	}
 	auto next_frame = clock::now();
 	int count = 0;
-	// while(count < 5) {
+	while(count < 5) {
+		show_cursor(false);
 		auto tickDur = std::chrono::duration<double>(1.0 / double(tickrate));
 		next_frame += std::chrono::duration_cast<clock::duration>(tickDur);
 		count++;
-		// if(!world.alive_count()) continue;
+		if(!world.alive_count()) continue;
 		world.physics();
 		render();
 		std::this_thread::sleep_until(next_frame);
-	// }
-		resetcolor();
+	}
+	resetcolor();
+	clearscreen();
+	show_cursor(true);
+	movecursor(0,0);
 }
 
 void Game::render() {
 	clearscreen();
 	Ps particles = world.getParticles();
 	for(const auto& p : particles) {
+		movecursor(std::floor(p->get_row()), std::floor(p->get_col()));
 		setbgcolor(p->get_r(), p->get_g(), p->get_b());
 		std::cout << " ";
-		movecursor(p->get_row(), p->get_col());
-		// resetcolor();
+		resetcolor();
 	}
 }
