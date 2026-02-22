2d99f25785b797dac4d55541bfd3d6030f725832
ho_1041841 <1041841@my.scccd.edu>
2026-02-21 22:11:07 -0800

    Bifrost header finalized? Hope so :))

--- Bifrost.cc
+++ Bifrost.cc
@@ -1,1 +1,12 @@
 #include "Bifrost.h"
+
+Bifrost::Bifrost(const Param &api_key, const Param &username)
+	: api_key(api_key), username(username), bridge(username, api_key) {
+}
+
+// Getters OUTSIDE Bifrost
+wXY Bifrost::getWorldBound(const WorldSnapshot &ws) {
+	Wc x = ws.get_rows();
+	Wc y = ws.get_cols();
+	return {x, y};
+}
--- Bifrost.h
+++ Bifrost.h
@@ -12,28 +12,50 @@
 // Typedef galore
 //===================================
 
+// 1) Aliased typedef
+
 // typedef from BRIDGES lib
 using Bridge = bridges::Bridges;
 using BColor = bridges::datastructure::Color;
 using BColorGrid = bridges::datastructure::ColorGrid;
 
-// typedef carried over from Particle.h
+// typedef carried over from Particle.h World.h
 // for clarity purpose
 using ColorValue = Color;
-using CoordsValue = Pc;
+using ParticleCoordsValue = Pc;
+using WorldCoordsValue = Wc;
 
-// typedef for color and particles from World
-typedef struct ParticleColor {
+// typedef for color and particles and size from World
+using ParticleColor = struct {
 	ColorValue red{}, green{}, blue{};
 };
 
-typedef struct ParticleCoords {
-        CoordsValue row{}, col{};
+using ParticleCoords = struct {
+	ParticleCoordsValue row{}, col{};
+};
+
+using ParticleState = struct {
+	ParticleCoords xy{};
+	ParticleColor rgb{};
+};
+
+using WorldCoords = struct {
+	WorldCoordsValue row{}, col{};
 };
 
 using pRGB = ParticleColor;
 using pXY = ParticleCoords;
+using pState = ParticleState;
+using wXY = WorldCoords;
 
+// 2) Internal typedef
+
+// input param typedef
+using WorldSnapshot = World;
+using Param = const std::string;
+
+// output typedef
+using VisualizerURL = std::string;
 
 //==============================
 // The class itself
@@ -42,19 +64,45 @@ using pXY = ParticleCoords;
 class Bifrost {
 private:
 	// Bridge-related member vars
-	const std::string api_key;
-	const std::string username;
-	Bridge bridge; //instantiate in .cc Todo
+	Param api_key, username;
+	Bridge bridge; // instantiate in .cc Todo
 
 	// Internal constructor. Singular.
-	Bifrost(const std::string &api_key, const std::string &username);
+	Bifrost(Param &api_key, Param &username);
 
-	//==============================
-	// Auxilirary functions
-	//==============================
+	// Getters OUTSIDE Bifrost
+	static wXY getWorldBound(const WorldSnapshot &ws);
+
 public:
+	// Convention for op func:
+	// - operator<< means insert
+	// - operator<<= means "assign partially/narrowing",
+	//   involving type/data loss (it looks like a funnel)
+	// - operator<= means total conversion (it looks like
+	//  an arrow)
+
+	// Chainable ops would return NOT bool type!
+
+	// BCG <<= ws
+	// Map color of a world snapshot.
+	friend bool operator<<=(BColorGrid &bcg, WorldSnapshot &ws);
+
+	// of stage (1), particle.rgb into BColor
+	friend bool operator<=(BColor &bc, const pRGB &rgb);
+
+	// bcg << (particle <<= p_ptr) syntax
+	friend pState &operator<<=(pState &particle, const P_ptr &p);
+
+	// two stages:
+	// (1) resolve color -> color and xy -> x y.
+	// (2) feed into BCG
+	friend bool operator<<(BColorGrid &bcg, const pState &particle);
+
+	// url <= BCG
+	friend bool operator<=(VisualizerURL &url, const BColorGrid bcg);
+
 	// Out-of-the-box BRIDGES visualizer
-	std::string visualize(World &world_snapshot);
+	friend bool operator<<=(VisualizerURL &url, WorldSnapshot &ws);
 };
- 
+
 #endif
